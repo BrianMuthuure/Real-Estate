f17e23897936af5cfd78a1b66d8228b787a67230
Brian Muthuure <brian@untapped-global.com>
2022-09-06 15:22:36 +0300

    feat:add properties and enquiries

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -53,7 +53,9 @@ LOCAL_APPS = [
     "apps.common",
     "apps.users",
     "apps.profiles",
-    "apps.ratings"
+    "apps.ratings",
+    "apps.properties",
+    "apps.inquiries"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
